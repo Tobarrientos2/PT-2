117bc3ca53a68959c08c2278a0116c1a9d88e4c2
Tomás Barrientos Stocker <tobarrientos2@gmail.com>
2024-03-09 11:59:06 -0300

    eliminated global variables

--- src/lib/Calculator.svelte
+++ src/lib/Calculator.svelte
@@ -5,25 +5,37 @@
     export let v_txt_2;
     export let arr_arr;
 
-    let input = '';
-    function handleNum(num_i:number){
-        let num_p = num_i.toString();
-        input += num_p;
+    let x:HTMLInputElement;
+    export function num_handleNum_txt(num_i:number){
+        let txt_o = '';
+
+        let txt_p = num_i.toString();
+        x.value += txt_p;
+        txt_o = x.value;
+        return txt_o;
     } 
 
-    function handleOperator(txt_i: string){
-        let txt_p = txt_i;
-        input += txt_p
+    export function txt_handleOperator_txt(txt_i: string){
+       let txt_o = '';
+       x.value += txt_i;
+       txt_o = x.value;
+       return txt_o;
     };
 
+    export function txt_handleEqual_txt(txt_i: string){
+        //If input has operators 
+            //Read input 
 
+            //If there's not operator then return the input
+            //If there's operator then process
+    }; 
     
 
 
 </script>
 
 
-<input type="text" bind:value={input} role="textbox">
+<input type="text" bind:this={x} role="textbox">
 <p>{v_txt_1}</p>
 <p>{v_txt_2}</p>
 
@@ -32,19 +44,20 @@
         {#each arr.arr_arr_num as arr_num }
             <div role="row">
                 {#each arr_num as num}
-                    <button on:click={() => handleNum(num)}>{num}</button>
+                    <button on:click={() => num_handleNum_txt(num)}>{num}</button>
                 {/each}
             </div>
         {/each}
     {/if}
     {#if arr.arr_txt}
         {#each arr.arr_txt as txt}
-            <button on:click={() =>{handleOperator(txt)}} >{txt}</button>
+            <button on:click={() =>{txt_handleOperator_txt(txt)}} >{txt}</button>
         {/each}
     {/if}
     {#if arr.arr_txt_2}
             {#each arr.arr_txt_2 as txt}
-                <button on:click={()=> utils.m_txt_1EvaluateAll_num(input)} >{txt}</button>
+            <!-- Se muestra el boton = -->
+                <button on:click={()=> txt_handleEqual_txt(input)} >{txt}</button>
             {/each}
     {/if}
 {/each}
